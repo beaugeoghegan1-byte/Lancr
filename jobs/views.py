382ec15ff4c2599f003114e50a5053c8225bb923
B3AU <232670984+beaugeoghegan1-byte@users.noreply.github.com>
2026-06-11 15:18:34 +0100

    bust cache to run seed

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,4 @@
+#comment
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden, HttpResponse, HttpResponseBadRequest
