16151b6dc4c532c36e14c47865c2344e012da8a7
B3AU <232670984+beaugeoghegan1-byte@users.noreply.github.com>
2026-06-12 12:34:41 +0100

    database

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600
     )
 }
